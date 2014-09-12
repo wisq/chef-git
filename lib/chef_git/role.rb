bf484f13b6bff71a5f05260d146d0ed90743a8ab
Adrian Irving-Beer <wisq@wisq.net>
2014-09-11 21:31:57 -0400

    Support multiple :role_paths.

--- lib/chef_git/role.rb
+++ lib/chef_git/role.rb
@@ -2,7 +2,10 @@ require 'chef/role'
 
 class ChefGit::Role < Chef::Role
   def self.from_disk(name, *args)
-    path = File.join(Chef::Config[:role_path], name.gsub('--', '/') + ".rb")
+    file = name.gsub('--', '/') + ".rb"
+    path = Array(Chef::Config[:role_path]).map { |dir| File.join(dir, file) }.detect { |f| File.exist?(f) }
+    raise "#{file.inspect} not found in :role_path #{role_path.inspect}" unless path
+
     role = new
     role.name(name)
     role.from_file(path)
