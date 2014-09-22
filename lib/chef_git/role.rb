ee76773944d54fe666d3772a5f58e1a81918af95
Adrian Irving-Beer <wisq@wisq.net>
2014-09-22 13:04:08 -0400

    Fix role error handling code.

--- lib/chef_git/role.rb
+++ lib/chef_git/role.rb
@@ -2,8 +2,9 @@ require 'chef/role'
 
 class ChefGit::Role < Chef::Role
   def self.from_disk(name, *args)
+    role_path = Chef::Config[:role_path]
     file = name.gsub('--', '/') + ".rb"
-    path = Array(Chef::Config[:role_path]).map { |dir| File.join(dir, file) }.detect { |f| File.exist?(f) }
+    path = Array(role_path).map { |dir| File.join(dir, file) }.detect { |f| File.exist?(f) }
     raise "#{file.inspect} not found in :role_path #{role_path.inspect}" unless path
 
     role = new
