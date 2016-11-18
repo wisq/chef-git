e7113cc916c3bf93ddd11cda1c9bedf271897e66
David Radcliffe <radcliffe.david@gmail.com>
2016-11-18 11:47:14 -0500

    monkey patch data_bag() method to use local data bags

--- lib/chef_git.rb
+++ lib/chef_git.rb
@@ -5,12 +5,14 @@ end
 
 require 'chef_git/expand_node_object'
 require 'chef_git/role'
+require 'chef_git/data_bag'
 
 original_verbosity = $VERBOSE
 begin
   $VERBOSE = nil # suppress warnings
   Chef::PolicyBuilder::ExpandNodeObject = ChefGit::ExpandNodeObject
   Chef::Role = ChefGit::Role
+  Chef::DataBag = ChefGit::DataBag
 ensure
   $VERBOSE = original_verbosity
 end
